eed4cf3e299ac8237590d04d9c1e8433600cc886
Luiza Fernandes <luhqfernandes@gmail.com>
2021-06-22 08:04:20 -0300

    All enspoints added

--- src/main/java/com/cadastroestudantes/controller/StudentController.java
+++ src/main/java/com/cadastroestudantes/controller/StudentController.java
@@ -8,13 +8,19 @@ import com.cadastroestudantes.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/students")
+@CrossOrigin
 
 public class StudentController {
     @Autowired
@@ -34,5 +40,27 @@ public class StudentController {
         Optional<Student> op = repo.findById(id);
         Student student = op.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         return student;
+    }
+
+    @PostMapping
+    public Student salvar(@RequestBody Student student){
+        student = repo.save(student);
+        return student;
+    }
+
+    @PutMapping("{id}")
+    public Student alterar(@RequestBody Student updateStudent, @PathVariable Long id){
+        Optional<Student> op = repo.findById(id);
+        Student student = op.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        student.setName(updateStudent.getName());
+        repo.save(student);
+        return student;
+    }
+
+    @DeleteMapping("{id}")
+    public void remover(@PathVariable Long id){
+        Optional<Student> op = repo.findById(id);
+        Student student = op.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        repo.delete(student);
     }
 }
